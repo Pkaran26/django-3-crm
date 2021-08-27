0bbe186c294153ba38a4c53efd10d6ac6c0440a9
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2021-08-27 17:56:18 +0530

    image upload

--- crm/settings.py
+++ crm/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'crm/static'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'crm/static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
